249d2d486935b40d5f99d21deb180fe75653a295
Garrett Sanders <sgarrettsanders@gmail.com>
2018-08-15 22:07:24 -0400

    pipe json object to file

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
 const request = require('request');
 const through = require('through2');
+const JSONStream = require('JSONStream');
 
 const POE_URL = 'http://api.pathofexile.com/public-stash-tabs'
-const outputFile = __dirname + '/data.txt';
+const OUTPUT_FILE = __dirname + '/data.txt';
+const JSON_FILE = __dirname + '/data.json';
 
 function requestStashes(url) {
-  request(url).pipe(fs.createWriteStream(outputFile));
+  request(url).pipe(fs.createWriteStream(OUTPUT_FILE));
 }
 
-requestStashes(POE_URL);
+// parse stash data
+const stream = through({objectMode: true}, function(buffer, _, next) {
+  this.push(buffer);
+  next();
+});
+
+fs.createReadStream(OUTPUT_FILE)
+  .pipe(JSONStream.parse('*'))
+  .pipe(stream)
+  .pipe(JSONStream.stringify('[\n', '\n', '\n]\n'))
+  .pipe(fs.createWriteStream(JSON_FILE));
