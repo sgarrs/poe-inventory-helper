c11036332f2bb6d5a2d2b19637b3e8220910d0d0
Garrett Sanders <sgarrettsanders@gmail.com>
2018-08-18 18:15:59 -0400

    iterate over stash data; sort unique item categories

--- stashes.js
+++ stashes.js
@@ -1,16 +1,33 @@
 const data = require('./data.json')[1];
+const _ = require('lodash');
 
-const PUBLIC_STASHES = data.filter((stash) => stash.public);
+const PUBLIC_STASHES = data.filter((stash) => stash.public); // array of stashes
+const PUBLIC_STASH_ITEMS = getStashItems(PUBLIC_STASHES); // array of stash arrays of item objects
+const ITEMS = _.flatten(PUBLIC_STASH_ITEMS);
+const CATEGORIES = getCategories(ITEMS);
 
-const items = () => {
+function getCategories(items) {
+  let categoryArray = [];
+  items.forEach((item) => categoryArray.push(item.category));
+
+  let sortedCategories = [];
+  categoryArray.forEach((obj) => sortedCategories.push(Object.getOwnPropertyNames(obj)));
+  sortedCategories = _.flatten(sortedCategories);
+  sortedCategories = _.uniq(sortedCategories).sort();
+
+  return sortedCategories;
+}
+
+function getStashItems(data) { // separate the item data from parent objects
   const itemArray = [];
 
-  PUBLIC_STASHES.forEach((stash) => {
-    itemArray.push(stash.items); 
+  data.forEach((stash) => {
+    itemArray.push(stash.items);
   });
 
   return itemArray;
-
 }
-console.log(items);
 
+// const item = new Item(ITEMS[0]);
+
+console.log(CATEGORIES);
