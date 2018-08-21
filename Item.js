fd7f7be3c446df93de8e394d1de0dde8d0f2e529
Garrett Sanders <sgarrettsanders@gmail.com>
2018-08-20 22:06:36 -0400

    start fleshing out Item model: clean markup in typeLine text

--- Item.js
+++ Item.js
@@ -1,13 +1,20 @@
+const {
+  cleanTypeLine,
+} = require('./helpers.js');
+
 module.exports = function Item(data) {
   this.ilvl = data.ilvl;
   this.icon = data.icon;
   this.league = data.league;
   this.name = data.name;
-  this.typeLine = data.typeLine;
+  this.typeLine = cleanTypeLine(data);
   this.identified = data.identified;
-  this.requirements = [data.requirements];
-  this.implicitMods = [data.implicitMods];
-  this.explicitMods = [data.explicitMods];
+  this.requirements = data.requirements;
+  this.mods = {
+    implicit: data.implicitMods,
+    explicit: data.explicitMods,
+  };
+  this.flavorText = data.flavourText; // 'murica
   this.category = data.category;
   this.inventoryId = data.inventoryId;
 };
